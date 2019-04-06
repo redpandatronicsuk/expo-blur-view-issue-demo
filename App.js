27e6c70984957208cf670f2715472dc2c77235dc
Bombi Bombi <dominik.beste@gmail.com>
2019-04-06 15:37:28 +0200

    also demo for expo-av bug now

--- App.js
+++ App.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import { BlurView } from 'expo-blur'
+import { Audio } from 'expo-av'
+
+const demoAudio = require('./assets/demo.mp3')
 
 export default class App extends Component {
+
+  state = {
+    isAudioReady: false,
+    isAudioPlaying: false
+  }
+  soundObj = null
+
+  componentDidMount = async () => {
+    await this.setupAudio()
+  }
+
+  setupAudio = async () => {
+    await Audio.setIsEnabledAsync(true)
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: true,
+      playsInSilentLockedModeIOS: true,
+      playsInSilentModeIOS: true,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      playThroughEarpieceAndroid: false,
+      staysActiveInBackground: true // Ignored on iOS 😭
+    })
+    this.soundObj = await Audio.Sound.createAsync(
+      demoAudio,
+      { shouldPlay: false }
+    )
+    this.setState({ isAudioReady: true })
+  }
+
+  togglePlayPause = async () => {
+    const { isAudioPlaying } = this.state
+    if (isAudioPlaying) {
+      await this.soundObj.sound.pauseAsync()
+      this.setState({ isAudioPlaying: false })
+    } else {
+      await this.soundObj.sound.playAsync()
+      this.setState({ isAudioPlaying: true })
+    }
+  }
+
   render() {
+    const { isAudioReady, isAudioPlaying } = this.state
     return (
       <View style={styles.container}>
         <BlurView intensity={100} style={[StyleSheet.absoluteFill, styles.blur]}>
           <Text>Not blurred</Text>
+          {
+            isAudioReady ?
+            <TouchableOpacity onPress={this.togglePlayPause}>
+              <Text>{ isAudioPlaying ? 'PAUSE' : 'PLAY' }</Text>
+            </TouchableOpacity>
+            :
+            <ActivityIndicator />
+          }
         </BlurView>
         
         <Text style={styles.instructions}>¿Blurred?</Text>
---
+++ README.md
@@ -0,0 +1,16 @@
+# Demo for expo issue #3935
+Demo for issue blur-view not working in bare Expo projects.
+
+## Update
+
+Also demonstrating `staysActiveInBackground` `expo-av` bug on iOS.
+
+### Set up for AV bug demo
+You need to apply a workaround to prevent `expo-av` from crashing. Open up `ios/blurexample/AppDlegate.m` and change ~line 36
+```
+NSArray<id<RCTBridgeModule>> *extraModules = [_moduleRegistryAdapter extraModulesForBridge:bridge andExperience:nil];
+```
+to
+```
+NSArray<id<RCTBridgeModule>> *extraModules = [_moduleRegistryAdapter extraModulesForBridge:bridge andExperience:@"FOO"];
+```
